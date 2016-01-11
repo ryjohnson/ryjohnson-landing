81d96643c25c95d7582429712717032880713ee9
Ryan Johnson <ry.johnson@gmail.com>
2016-01-11 16:25:21 -0500

    Masked Email Address

--- _src/scripts/main.js
+++ _src/scripts/main.js
@@ -1,5 +1,15 @@
 $(function() {
   var currentYear = new Date().getFullYear();
-
   $('.copyright').append(currentYear);
+
+  $(window).on('load', function() {
+    var user = 'hello',
+        domain = 'ryjohnson.com';
+
+    $('footer a').append(user + '@' + domain);
+
+    $('footer a').on('click', function() {
+      window.location.href = 'mailto:' + user + '@' + domain;
+    });
+  });
 });
